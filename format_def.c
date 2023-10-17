07c7ce6d2cfe95da0e4e8454da82ffa06fce3406
PandaBooBoo <aaronchinzila@gmail.com>
2023-10-17 15:37:40 -0700

    format_def updated/fix

--- format_def.c
+++ format_def.c
@@ -67,3 +67,45 @@ void print_di(int n)
 	}
 	_putchar(m % 10 + '0');
 }
+
+/**
+ * int_counter - counter for len of int
+ * @n: the int to print
+ *
+ *
+ * Return: int
+ */
+int int_counter(int n)
+{
+	int count = 0;
+
+	if (n <= 0)
+		count++;
+	while (n != 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
+}
+/**
+ * print_revs - recursively prints strings in reverse
+ * @str: string
+ *
+ * Return: int
+ */
+int print_revs(char *str)
+{
+	int i = 0;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	for (i = (count - 1); i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	return (count);
+}
+
