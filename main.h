12ce6bd85525239e4d0cb7f8d43cc451995c0971
PandaBooBoo <aaronchinzila@gmail.com>
2023-10-17 15:19:25 -0700

    Header file update

--- main.h
+++ main.h
@@ -6,3 +6,12 @@
 int _putchar(char c);
 int print_c (int character);
 int print_str(char *str);
+int print_revs(char *str);
+int print_per(void);
+int _printf(char *format, ...);
+int _printf_ext_1(va_list val, char ch, unsigned int *p_i);
+void print_di(int n);
+int int_counter(int n);
+
+#endif
+
